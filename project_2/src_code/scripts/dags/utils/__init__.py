e085e2dbc072df051b0cef657e9fd481cde5589c
Amara <amara.mailpro@gmail.com>
2025-12-14 00:03:59 +0100

    refactoring

--- project_2/src_code/scripts/dags/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Utils package for Airflow DAGs
-Contains helper classes and functions for the ETL pipeline.
-"""
-
-from .mongo_handler import MongoHandler
-from .review_processor import ReviewProcessor
-
-__all__ = ['MongoHandler', 'ReviewProcessor']
